572b16c28b5647cddd2e827696683e6393b95ef7
wildlifehexagon <crossthebreeze@gmail.com>
2018-02-06 19:51:17 -0600

    switch from commander to yargs for argument parsing

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-const fs = require('fs')
-const request = require('request')
-const program = require('commander')
+// const fs = require('fs')
+// const request = require('request')
+// const convertFromRaw = require('draft-js').convertFromRaw
+const yargs = require('yargs')
 
-program
-    .version('1.0.0', '-v, --version')
-    .description('Content loader script for the dictyBase API')
-    .command('upload [-f|--file=<file>] [-s|--server=<server>] [-n|--namespace=<namespace>]')
-    .alias('u')
+yargs
+    .usage('$0 <cmd> [args]')
+    .alias('v', 'version')
+    .version('1.0.0')
+    .describe('v', 'show version information')
 
-program.parse(process.argv)
+    .command(
+        'upload [file] [server] [namespace]',
+        'input file to upload with specified server and namespace',
+        yargs => {
+            yargs
+                .positional('file', {
+                    alias: 'f',
+                    type: 'string',
+                    default: 'index',
+                    describe: 'the file to upload'
+                })
+                .positional('server', {
+                    alias: 's',
+                    type: 'string',
+                    default: 'http://localhost:3000',
+                    describe: 'the server to upload to'
+                })
+                .positional('namespace', {
+                    alias: 'n',
+                    type: 'string',
+                    default: 'index', // change to filename
+                    describe: 'the namespace for the file'
+                })
+        },
+        function(argv) {
+            console.log(argv.file, argv.server, argv.namespace)
+        }
+    )
+    .help()
+    .example(
+        'node index upload --file example.json --server localhost:3000 --namespace example'
+    )
+    .example('node index upload -f example.json -s localhost:3000 -n example')
+    .argv
