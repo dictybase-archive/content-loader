194d62f32796e71aebd8a62a0f3ec317a50d4370
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2018-02-10 11:38:16 +0000

    use fetch and async-await for retrieving content

--- cmds/fetch.js
+++ cmds/fetch.js
@@ -1,4 +1,4 @@
-const http = require('http')
+const fetch = require('node-fetch')
 
 exports.command = 'fetch [id] [host] [port]'
 exports.describe = 'retrieve content by id'
@@ -35,27 +35,45 @@ exports.builder = yargs => {
 
 exports.handler =  argv => {
         // set options for HTTP GET request
-        const options = {
-            host: argv.host,
-            port: argv.port,
-            path: '/contents/' + argv.identifier,
-            method: 'GET'
+        const url = `http://${argv.host}:${argv.port}/contents/${argv.identifier}`
+        getContent(url)
+}
+
+// An async function(search google for syntaxes)
+const getContent = async (url) => {
+    try {
+        // get the response(resolves the first promise)
+        res = await fetch(url)
+        if (res.ok) { // successful http response(read the API spec)
+            // now get the json(resolves the second promise)
+            json = await res.json()
+            printContent(json)
+        } else { // this is an http error(error response from server)
+            // comes in JSONAPI error format(http://jsonapi.org/examples/#error-objects-basics)
+            json = await res.json() // this is the error json(same promise)
+            printError(res,json)
         }
+    } catch (err) {
+        //possibly a network error or something
+        console.log(`network error: ${err.message}`)
+    }
 
-        // make the HTTP request
-        let req = http.request(options, res => {
-            let body
-            res.on('data', chunk => {
-                body += chunk
-            })
-            res.on('end', () => {
-                c = JSON.parse(body)
-            })
-        })
+}
 
-        // notify of potential error messages
-        req.on('error', e => {
-            console.log(`Problem with request: ${e}`)
-        })
-        req.end()
+const printContent = (json) => {
+    console.log(
+        `resource link: ${json.links.self}
+        namespace: ${json.data.attributes.namespace}
+        slug: ${json.data.attributes.slug}
+    `)
+}
+
+const printError = (res,json) => {
+    console.log('got http error******')
+    console.log(
+        `http response: ${res.status}
+         title: ${json.errors[0].title}
+         detail: ${json.errors[0].detail}
+        `)
+
 }
