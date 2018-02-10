83105c5a2994822a5129f912c56263656f36eda2
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2018-02-10 01:37:10 +0000

    Added fetch subcommand

---
+++ cmds/fetch.js
@@ -0,0 +1,65 @@
+const http = require('http')
+
+exports.command = 'fetch [id] [host] [port]'
+exports.describe = 'retrieve content by id'
+exports.builder = yargs => {
+    yargs
+        .env('CONTENT_API_SERVICE_HOST')
+        .positional('host', {
+            alias: 'H',
+            type: 'string',
+            default: 'content-api',
+            describe: 'api server'
+        })
+        .env('CONTENT_API_SERVICE_PORT')
+        .positional('port', {
+            alias: 'p',
+            type: 'number',
+            describe: 'api server port'
+        })
+        .positional('identifier', {
+            alias: 'id',
+            type: 'number',
+            describe: 'unique identifier for the content'
+        })
+        .demandOption(['id'])
+        .help('h')
+        .example(
+            'fetch --identifier 3 --host localhost --port 31827'
+        )
+        .example(
+            'fetch -id 3 -H localhost -p 31827'
+        )
+}
+
+
+exports.handler =  argv => {
+        // set options for HTTP GET request
+        const options = {
+            host: argv.host,
+            port: argv.port,
+            path: '/contents/' + argv.identifier,
+            headers: {
+                'Content-Type': 'application/json',
+                'accept': 'application/json'
+            }
+        }
+
+        // make the HTTP request
+        let req = http.request(options, res => {
+            console.log(`status: ${res.statusCode}\n`)
+            res.setEncoding('utf8')
+            let body
+            res.on('data', chunk => {
+                body += chunk
+            })
+            res.on('end', () => {
+                console.log(body)
+            })
+        })
+
+        // notify of potential error messages
+        req.on('error', e => {
+            console.log(`Problem with request: ${e}`)
+        })
+}
