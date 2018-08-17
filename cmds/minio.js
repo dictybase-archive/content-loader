7598d0c56358bb02664c3a7624318c1f84e6a65b
wildlifehexagon <crossthebreeze@gmail.com>
2018-08-17 15:30:45 -0500

    integrate uploading into minio script, currently tries to upload too many times

--- cmds/minio.js
+++ cmds/minio.js
@@ -1,12 +1,15 @@
+const fs = require("fs")
 const Minio = require("minio")
 const yargs = require("yargs")
 const bunyan = require("bunyan")
 const { Writable } = require("stream")
 const filepath = require("path")
+const fetch = require("node-fetch")
+const moment = require("moment")
 const tmp = require("tmp")
 
 const argv = yargs // eslint-disable-line
-  .usage("Usage: $0 [options] - list all objects in the given path")
+  .usage("Usage: $0 [options] - download data from minio and upload to content API server")
   .option("host", {
     alias: "H",
     type: "string",
@@ -35,16 +38,125 @@ const argv = yargs // eslint-disable-line
     type: "string",
     describe: "folder path",
   })
-  .demandOption(["H", "P", "secret", "access", "p", "b"])
+  .env("CONTENT_API_SERVICE")
+  .option("chost", {
+    type: "string",
+    default: "content-api",
+    describe: "the server to upload to",
+  })
+  .env("CONTENT_API_SERVICE")
+  .option("cport", {
+    type: "number",
+    describe: "the port for the server",
+  })
+  .option("namespace", {
+    alias: "n",
+    type: "string",
+    describe: "the namespace for the folder",
+  })
+  .option("user", {
+    alias: "u",
+    type: "number",
+    describe: "the user who is uploading the content",
+  })
+  .demandOption(["H", "P", "secret", "access", "p", "b", "chost", "cport", "n", "u"])
   .help("h")
   .example(
-    "$0 -H play.minio.io --port 9000 --secret 48kjpqr3u --access furiwer02 -b mybucket -p /content",
+    "$0 -H play.minio.io --port 9000 --secret 48kjpqr3u --access furiwer02 -b mybucket -p /content --chost 192.168.99.100 --cport 30999 -n dsc -u 999",
   ).argv
 
+const getLogger = () => {
+  return bunyan.createLogger({ name: "listobject" })
+}
+
+const tmpObj = tmp.dirSync({ prefix: "minio-" })
+const folder = tmpObj.name
+
+const printContent = json => {
+  const logger = getLogger()
+  let output = `resource link: ${json.links.self}
+      id: ${json.data.id}
+      namespace: ${json.data.attributes.namespace}
+      slug: ${json.data.attributes.slug}
+         `
+  const created = moment(json.data.attributes.created_at)
+  if (created.isValid()) {
+    output += `created on: ${created.fromNow()}`
+  } else {
+    logger.info("error in parsing date")
+  }
+  logger.info(output)
+}
+
+const printError = (res, json) => {
+  const logger = getLogger()
+  logger.error(
+    `http response: ${res.status}
+           title: ${json.errors[0].title}
+           detail: ${json.errors[0].detail}
+          `,
+  )
+}
+
+const postContent = async (url, body) => {
+  const logger = getLogger()
+  try {
+    // get the response(resolves the first promise)
+    const res = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify(body),
+    })
+    if (res.ok) {
+      // successful http response
+      // now get the json (resolves the second promise)
+      const json = await res.json()
+      printContent(json)
+    } else {
+      // this is an http error (error response from server)
+      // comes in JSONAPI error format
+      const json = await res.json() // this is the error json (same second promise)
+      printError(res, json)
+    }
+  } catch (err) {
+    // possibly a network error or something
+    logger.error(`network error: ${err.message}`)
+  }
+}
+
+const uploadFiles = () => {
+  const logger = getLogger()
+
+  // read folder
+  fs.readdir(`${folder}`, (err, files) => {
+    if (err) {
+      logger.error(err)
+      process.exit(1) // stop the script
+    }
+    // for each file in folder, run this script
+    files.forEach(file => {
+      // read file and convert to string
+      const fileContent = fs.readFileSync(`${folder}/${file}`).toString()
+      const url = `http://${argv.chost}:${argv.cport}/contents`
+      // set object to match dictybase content API
+      const body = {
+        data: {
+          type: "contents",
+          attributes: {
+            name: filepath.basename(file, filepath.extname(file)),
+            created_by: argv.user,
+            content: fileContent,
+            namespace: argv.namespace,
+          },
+        },
+      }
+      postContent(url, body)
+    })
+  })
+}
+
 class S3Writer extends Writable {
-  constructor({ client, folder, bucket, logger }) {
+  constructor({ client, bucket, logger }) {
     super({ objectMode: true })
-    this.folder = folder
     this.client = client
     this.bucket = bucket
     this.logger = logger
@@ -52,18 +164,16 @@ class S3Writer extends Writable {
 
   async _write(object, _, done) {
     try {
-      const fullPath = filepath.join(this.folder, filepath.basename(object.name))
+      const fullPath = filepath.join(folder, filepath.basename(object.name))
       await this.client.fGetObject(this.bucket, object.name, fullPath)
       this.logger.info("saved %s", fullPath)
     } catch (error) {
       this.logger.error("unable to write the file %s", object.name)
     }
     done()
+    // below is the problematic line -- how to upload files only after ALL files are downloaded?
+    uploadFiles()
   }
-}
-
-const getLogger = () => {
-  return bunyan.createLogger({ name: "listobject" })
 }
 
 const getS3Client = options => {
@@ -81,8 +191,6 @@ const listObjects = options => {
   const client = getS3Client(options)
   const { bucket, path } = options
   const stream = client.listObjects(bucket, path, true)
-  const tmpObj = tmp.dirSync({ prefix: "minio-" })
-  const folder = tmpObj.name
   stream.pipe(
     new S3Writer({
       client,
